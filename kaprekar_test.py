0dce649dccb3d3ea303f3139f40fd2d4bcd7cc6f
austingandy <austin.gandy@gmail.com>
2016-12-29 14:34:17 -0800

    added readme and histogram

---
+++ kaprekar_test.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import numpy as np
+
+def find_low(num):
+  digits = get_digits(increase(num))
+  digits.sort()
+  return int(''.join(digits))
+
+def find_high(num):
+  digits = get_digits(increase(num))
+  digits.sort()
+  digits.reverse()
+  return int(''.join(digits))
+
+def increase(num):
+  while (num < 1000):
+    num *= 10
+  return num
+
+def get_digits(num):
+  return [i for i in str(num)]
+
+KAPREKAR_NUM = 6174
+counts = np.zeros(10000)
+hist = defaultdict(lambda: 0)
+for i in range(1000,10000):
+  if (i % 1111 == 0):
+    continue
+  diff, count = i, 0
+  while (diff != KAPREKAR_NUM):
+    diff = find_high(diff) - find_low(diff)
+    count += 1
+  counts[i] = count
+
+plt.hist(counts, 7, normed=1, facecolor='g', alpha=0.75)
+plt.xlabel('Iterations to Kaprekar Number')
+plt.ylabel('Frequency')
+#plt.bar(hist.keys(), hist.values(), 2.0, color='g')
+plt.show()
---
+++ readme.md
@@ -0,0 +1,2 @@
+This was inspired by [this](https://www.reddit.com/r/math/comments/5kvn7k/a_persistent_difference_6174/) post from the math subreddit.
+I was interested in seeing whether or not this claim was true and how many iterations this took to complete on average. Included is the code and generated histogram.
